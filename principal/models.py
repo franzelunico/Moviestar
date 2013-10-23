e2e0dfdcadf7aa9d5dd280e0374a4c8e2ba4adfc
Marcelo <marcelo.arevalo.azurduy@gmail.com>
2013-10-23 12:06:17 -0400

    Modelado de la primera historia de usuario, usando el modelo ER con Idioms

---
+++ principal/models.py
@@ -0,0 +1,33 @@
+#encoding:utf-8
+from django.db import models
+
+#Poner 3d y 2d como campo de seleccion (como sexo) para hacer mas eficiente la busqueda en la BD			
+class Formato(models.Model):
+	nombre_formato		= models.CharField(max_length=50)
+	def __unicode__(self):
+		return self.nombre_formato
+
+class Genero(models.Model):
+	nombre_genero		= models.CharField(max_length=50)
+ 	
+	def __unicode__(self):
+		return self.nombre_genero
+
+class Pelicula(models.Model):
+	titulo_pelicula		= models.CharField(max_length=100)
+	descripcion_pelicula	= models.TextField(max_length=500, help_text='Redacta la sinopsis de la pelicula')
+#	imagen_pel		= models.ImageField(upload_to='peliculas', verbose_name='imagen_pel')
+	genero_pelicula		= models.ForeignKey('Genero')
+	
+	def __unicode__(self):
+		return self.titulo_pelicula
+
+class Copia_Formato(models.Model):
+	cantidad_copia		= models.IntegerField()
+	precio_copia		= models.FloatField()
+	pelicula_copia		= models.ForeignKey('Pelicula')
+	formato_copia		= models.ForeignKey('Formato')
+
+	def __unicode__(self):
+		msg = '"'+ self.pelicula_copia.titulo_pelicula +'"' + ' en formato ' + self.formato_copia.nombre_formato
+		return msg
