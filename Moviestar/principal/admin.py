04288ec999ccab46e7d1b9377d34523082222205
franzelunico <franzelunico@gmail.com>
2013-10-28 16:26:32 -0400

    Cambios Marcelo

---
+++ Moviestar/principal/admin.py
@@ -0,0 +1,7 @@
+from principal.models import Genero, Formato, Pelicula, Copia_Formato
+from django.contrib import admin
+
+admin.site.register(Genero)
+admin.site.register(Formato)
+admin.site.register(Pelicula)
+admin.site.register(Copia_Formato)
