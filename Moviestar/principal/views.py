4dc33fdb3b558a18a16c844729a0be9f48f139bf
elvico <elvikoon@gmail.com>
2013-11-01 15:34:12 +0000

    views

--- Moviestar/principal/views.py
+++ Moviestar/principal/views.py
@@ -1,1 +1,17 @@
-# Write your views here
+from django.shortcuts import render_to_response
+from models import Pelicula
+from django.db.models import Q
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
+
+def search(request):
+    query = request.GET.get('q', '')
+    if query:
+        qset = (
+            Q(titulo_pelicula__icontains=query)
+        )
+        results = Pelicula.objects.filter(qset).distinct()
+    else:
+        results = []
+    return render_to_response("search.html",{"results": results, "query": query})
